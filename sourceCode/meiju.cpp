8b014aa65986ea6ec0c1064e2bb235f8733c9f23
chenwuxing <chenwuxing87@163.com>
2020-08-23 20:38:21 +0800

    枚举类型

---
+++ sourceCode/meiju.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum weather{sunny,cloudy,rainy,windy};
+    enum fruits{apple=3,orange,banana=7,bear};
+    cout<<orange<<endl;
+    enum big_cites{guangzhou=1,shenzhen=3,beijing=1,shanghai=2};
+    
+}
